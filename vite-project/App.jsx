26e09da306af1fcd489319f91ac37b57e7d8228c
qu <1095698022@qq.com>
2024-03-27 23:23:29 +0800

    feat: props

--- vite-project/App.jsx
+++ vite-project/App.jsx
@@ -1,17 +1,20 @@
 import React from "./core/React.js";
-
+let count = 1;
+const props = {id:"test"}
 function Counter({num}){
 function handleClick (){
     console.log('click')
+    count++
+    React.update()
 }
-    return (<div>count: {num}<button onClick={handleClick}>click</button></div>)
+    return (<div {...props}>count: {count}<button onClick={handleClick}>click</button></div>)
 }
 function CounterContainer(){
     return <Counter num={10}>count</Counter>
 }
 
 function App (){
-    return <div>hi,mini-react<Counter num={10}>count</Counter></div>
+    return <div>hi,mini-react<Counter >count</Counter></div>
 }
 
 // const App = <div>hi,mini-react<CounterContainer></CounterContainer>
--- vite-project/core/React.js
+++ vite-project/core/React.js
@@ -35,6 +35,7 @@ function render(el, container) {
 }
 
 let root = null;
+let currentRoot = null;
 
 let nextWorkOfUnit = null;
 function workLoop(deadline) {
@@ -57,6 +58,7 @@ function workLoop(deadline) {
 
 function commitRoot() {
   commitWork(root.child);
+  currentRoot = root;
   root = null;
 }
 
@@ -67,9 +69,15 @@ function commitWork(fiber) {
   while (!fiberParent.dom) {
     fiberParent = fiberParent.parent;
   }
-  if (fiber.dom) {
-    fiberParent.dom.append(fiber.dom);
+  if (fiber.effectTag == "update") {
+    // update
+    updateProps(fiber.dom, fiber.props, fiber.alternate?.props);
+  } else if (fiber.effectTag == "placement") {
+    if (fiber.dom) {
+      fiberParent.dom.append(fiber.dom);
+    }
   }
+
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
@@ -80,32 +88,77 @@ function createDom(type) {
     : document.createElement(type);
 }
 
-function updateProps(dom, props) {
-  Object.keys(props).forEach((key) => {
+function updateProps(dom, nextProps, prevProps) {
+  // Object.keys(nextProps).forEach((key) => {
+  //   // 只处理非children的props
+  //   if (key !== "children") {
+  //     if (key.startsWith("on")) {
+  //       //
+  //       const event = key.slice(2).toLowerCase();
+  //       dom.addEventListener(event, nextProps[key]);
+  //     } else {
+  //       dom[key] = nextProps[key];
+  //     }
+  //   }
+  // });
+  Object.keys(prevProps).forEach((key) => {
+    if (key !== "children") {
+      if (!(key in nextProps)) {
+        dom.removeAttribute(key);
+      }
+    }
+  });
+
+  Object.keys(nextProps).forEach((key) => {
     // 只处理非children的props
     if (key !== "children") {
-      if (key.startsWith("on")) {
-        //
-        const event = key.slice(2).toLowerCase();
-        dom.addEventListener(event, props[key]);
-      } else {
-        dom[key] = props[key];
+      if (nextProps[key] !== prevProps[key]) {
+        if (key.startsWith("on")) {
+          const eventType = key.slice(2).toLowerCase();
+
+          dom.removeEventListener(eventType, prevProps[key]);
+          dom.addEventListener(eventType, nextProps[key]);
+        } else {
+          dom[key] = nextProps[key];
+        }
       }
     }
   });
 }
 
 function initChildren(fiber, children) {
+  let oldFiber = fiber.alternate?.child;
   let prevChild = null;
   children.forEach((child, index) => {
-    const newFiber = {
-      type: child.type,
-      props: child.props,
-      child: null,
-      parent: fiber,
-      sibling: null,
-      dom: null,
-    };
+    const isSameType = oldFiber && oldFiber.type === child.type;
+    let newFiber;
+    if (isSameType) {
+      // isSameType
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null,
+        parent: fiber,
+        sibling: null,
+        dom: oldFiber.dom,
+        effectTag: "update",
+        alternate: oldFiber,
+      };
+    } else {
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null,
+        parent: fiber,
+        sibling: null,
+        dom: null,
+        effectTag: "placement",
+      };
+    }
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
     if (index === 0) {
       fiber.child = newFiber;
     } else {
@@ -115,20 +168,25 @@ function initChildren(fiber, children) {
   });
 }
 
-function updateFunctionComponent(fiber) {
-  //   if (!fiber.dom) {
-  //     const dom = (fiber.dom = createDom(fiber.type));
+// function updateFunctionComponent(fiber) {
+//   //   if (!fiber.dom) {
+//   //     const dom = (fiber.dom = createDom(fiber.type));
 
-  //     updateProps(dom, fiber.props);
-  //   }
+//   //     updateProps(dom, fiber.props);
+//   //   }
+//   const children = [fiber.type(fiber.props)];
+//   initChildren(fiber, children);
+// }
+function updateFunctionComponent(fiber) {
   const children = [fiber.type(fiber.props)];
+
   initChildren(fiber, children);
 }
 
 function updateHostComponent(fiber) {
   if (!fiber.dom) {
     const dom = (fiber.dom = createDom(fiber.type));
-    updateProps(dom, fiber.props);
+    updateProps(dom, fiber.props, {});
   }
   const children = fiber.props.children;
   initChildren(fiber, children);
@@ -200,8 +258,20 @@ function performUnitOfWork(fiber) {
 }
 
 requestIdleCallback(workLoop);
+//
+function update() {
+  //
+  nextWorkOfUnit = {
+    dom: currentRoot.dom,
+    props: currentRoot.props,
+    alternate: currentRoot,
+  };
+
+  root = nextWorkOfUnit;
+}
 
 const React = {
+  update,
   render,
   createElement,
 };
